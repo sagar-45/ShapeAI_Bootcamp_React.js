b07e128d069bdc5d2caf51a5aa4eebc345cbce1e
sagar-45 <63908993+sagar-45@users.noreply.github.com>
2021-06-05 05:20:00 -0700

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This bootcamp is organized by ShapeAI and it's a very amazing bootcamp.
+        In this bootcamp we learn about Javascript,HTML and React.js.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
---
+++ index.js
@@ -0,0 +1,11 @@
+import react from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
+
+
+ReactDom.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
